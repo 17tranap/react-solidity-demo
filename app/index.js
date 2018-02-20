6c368289fb9115e3b17d0e52302caabd34f53e76
alexandria <apt029@ucsd.edu>
2018-02-20 15:06:54 -0800

    body{     font-family: 'open sans';     margin: 0; } ul{     list-style-type: none;     padding-left: 0;     display: flex; } li{     padding: 40px;     border: 2px solid rgb(30,134,255);     margin-right: 5px;     border-radius: 10px;     cursor: pointer; } li:hover{     background-color: rgb(30,134,255);     color: white; } li:active{     opacity: 0.7; } *{    color: #444444; } .main-container{    padding: 20px; } .block{    display: flex;    align-items: center; } .number-selected{    background-color: rgb(30,134,255);    color: white; } .bet-input{    padding: 15px;    border-radius: 10px;    border: 1px solid lightgrey;    font-size: 15pt;    margin: 0 10px; } new index.css, index.js

--- app/index.js
+++ app/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Web3 from 'web3'
+import './../css/index.css'
 
 class App extends React.Component {
+	addValue(value) { 
+
+	}
 	render() {
 		return (
-	      <div className="solidity">
+	      <div className="main-conatiner">
+	        <h1>Contribute to the pot</h1>
 	      	<h3 class="label"> Test </h3>
 			<input type="text" id="name"/>
 			<button onClick='#'> Send </button>
