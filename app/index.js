7a854fb7a31427bc02b8bbfa4f3017de7e327a6e
alexandria <apt029@ucsd.edu>
2018-02-20 15:28:58 -0800

    pragma solidity ^0.4.19;
    contract Pot {
    	address owner;
    	uint total;
    	uint minimumContribution;
        address[] contributors;
    	function Pot(uint _minimumContribution) {
    		owner = msg.sender;
    		total = 0;
    		if(_minimumContribution != 0) minimumContribution = _minimumContribution;
    	}
    
    	function kill() {
    		if(msg.sender == owner)
    			selfdestruct(owner);
    	}
    
    	struct Contributor {
    		uint amountIn;
    		uint totalContributed;
    	}
    	mapping(address => Contributor) contributorInfo;
    
    	function checkContributorExists(address contributor) returns(bool) {
    		for(uint i = 0; i < contributors.length; i++) {
    			if(contributors[i] == contributor) return true;
    		}
    		return false;
    	}
    
    	function putInMoney(uint number) payable{
    		assert(checkContributorExists(msg.sender) == false);
    		assert(number >= 1 && number <=10);
    		assert(msg.value >= minimumContribution);
    
    		contributorInfo[msg.sender].amountIn = msg.value;
    		contributorInfo[msg.sender].totalContributed += msg.value;
    		contributors.push(msg.sender);
    		total += msg.value;
    
    	}
    
    	function returnMoney() {
    		for(uint i = 0; i < contributors.length; i++) {
    		    uint moneyIn = contributorInfo[contributors[i]].totalContributed;
    			contributors[i].transfer(moneyIn);
    		}
    	}
    
    	function () payable {}
    }
    pot should work

--- app/index.js
+++ app/index.js
@@ -4,15 +4,33 @@ import Web3 from 'web3'
 import './../css/index.css'
 
 class App extends React.Component {
+	constructor(props){
+	    super(props)
+	    this.state = {
+	      minimumContribution: 0,
+	      total: 0,
+	    }
+	    if(typeof web3 != 'undefined'){
+	      console.log("Using web3 detected from external source like Metamask")
+	      this.web3 = new Web3(web3.currentProvider)
+	    }else{
+	      this.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"))
+	    }
+	    const MyContract = web3.eth.contract([here goes the ABI interface])
+	    this.state.ContractInstance = MyContract.at("0x61d89802d850d2b18cb7d42afc9a39776b38eb97")
+	}
 	addValue(value) { 
 
 	}
 	render() {
 		return (
-	      <div className="main-conatiner">
+	      <div className="main-container">
 	        <h1>Contribute to the pot</h1>
-	      	<h3 class="label"> Test </h3>
-			<input type="text" id="name"/>
+	      </div>
+
+	      <div className="block">
+	      	<h3 class="label"> How musch do you want to be? </h3>
+			<input type="pot-input"  ref="ether-bet" type="number" placeholder={this.state.minimumContribuion} />
 			<button onClick='#'> Send </button>
 		  </div>
 	    );
