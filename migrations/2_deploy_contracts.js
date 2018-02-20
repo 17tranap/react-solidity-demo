5d7c2a561bfe46441b9457194d11473caa3d23a2
alexandria <apt029@ucsd.edu>
2018-02-20 14:45:49 -0800

    Pot.sol compileable, new 2_deploy_contracts.js

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,8 @@
+var Pot = artifacts.require(".Casino.sol");
+
+module.exports = function(deployer) {
+	deployer.deploy(web3.toWei(0.1, 'ether'), 100, {gas: 3000000});
+} //0.1 means minimumContribution is set to 0.1 ether 
+  //converted to wei which is 10^(-18) ether
+  //100 means 100 max contributors
+  //gas limit is 3000000
